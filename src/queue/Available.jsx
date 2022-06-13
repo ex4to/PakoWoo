e259fb678ac3cb295527f4b4d8b2834c2c3766e8
secretDzen <turretbox@gmail.com>
2022-06-13 23:28:57 +0700

    fix: linters fixes

--- src/queue/Available.jsx
+++ src/queue/Available.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { Cell } from "@vkontakte/vkui";
 
 const RAvailable = ({ subArray, itemSetter }) => {
-  return subArray.map((val, index) => (
-    <Cell key={index} onClick={() => itemSetter(val)}>
+  return subArray.map((val) => (
+    <Cell key={subArray.id} onClick={() => itemSetter(val)}>
       {val.subject} - {val.date}
     </Cell>
   ));
--- src/queue/Subscripted.jsx
+++ src/queue/Subscripted.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { Cell } from "@vkontakte/vkui";
 
 const RSubscripted = ({ subArray, itemSetter }) => {
-  return subArray.map((val, index) => (
-    <Cell onClick={() => itemSetter(val)} key={index}>
+  return subArray.map((val) => (
+    <Cell onClick={() => itemSetter(val)} key={subArray.id}>
       {val.subject} - {val.date}
     </Cell>
   ));
